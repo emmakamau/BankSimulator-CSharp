ffe2912b990edc5ea051bb1b04338547ad38e1c0
EmmaculateKamau <EmmaculateKamau@DESKTOP-DRBKMQI>
2022-06-15 08:20:17 +0300

    Add project files.

---
+++ BankSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+var Balances = new Dictionary<string,decimal>()
+{
+    {"Wanjiru",0 },
+    {"Juma",0 },
+    {"Linda",0 }
+};
+
+Console.WriteLine(Balances);
+foreach (var item in Balances)
+    Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+
